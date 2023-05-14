1209c8aaada86fdb7822b3347246e2fc0e6ae683
Rafael <rafaelreinertd@hotmail.com>
2023-05-14 14:03:42 -0300

    return validation errors

--- src/middlewares/validateSchema.middleware.js
+++ src/middlewares/validateSchema.middleware.js
@@ -2,7 +2,7 @@
 export function validationMiddleware(schema){
     return(req, res, next)=>{
         const validationLog=schema.validate(req.body,{abortEarly: false});
-        if(validationLog.error) return res.sendStatus(400);
+        if(validationLog.error) return res.status(400).send(validationLog.error.details.map(e=>e.message));
         res.locals.validated=validationLog.value;
         next();
     }
