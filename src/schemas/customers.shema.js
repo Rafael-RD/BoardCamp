399ee2988e7ace5c75d067f28639d70887f1c9c0
Rafael <rafaelreinertd@hotmail.com>
2023-05-14 14:09:04 -0300

    fix: customers schema cpf

--- src/schemas/customers.shema.js
+++ src/schemas/customers.shema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const customerSchema=Joi.object({
     name: Joi.string().min(1).required().trim(),
-    phone: Joi.number().min(10).max(11).required(),
-    cpf: Joi.number().min(11).max(11).required(),
+    phone: Joi.string().min(10).max(11).required(),
+    cpf: Joi.string().min(11).max(11).pattern(/^[0-9]+$/).required(),
     birthday: Joi.string().isoDate().required().trim(),
 });
